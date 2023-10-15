5eb71936bae678e838929250c5712872f6791fa4
lenovo <lenovo@DESKTOP-QE2KOS9>
2023-10-15 10:52:53 +0530

    Comitting hunger logic in test1

---
+++ src/test/java/stepdefinition/test1.java
@@ -0,0 +1,17 @@
+package stepdefinition;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hunger = "yes";
+		
+		if(hunger == "yes")
+			System.out.println("Food needed");
+		else
+			System.out.println("Ignore");
+
+	}
+
+}
